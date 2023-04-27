e5cbe045c0884a0d002d79f729f03480bfae99af
MohamedWalidxX <103504375+MohamedWalidxX@users.noreply.github.com>
2023-04-28 02:07:17 +0300

    radio_Buttons_walid

--- FlightReservationSys/connected.Designer.cs
+++ FlightReservationSys/connected.Designer.cs
@@ -30,12 +30,14 @@
         {
             this.customer_button_id = new System.Windows.Forms.Button();
             this.admin_button_id = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // customer_button_id
             // 
             this.customer_button_id.Location = new System.Drawing.Point(393, 182);
-            this.customer_button_id.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.customer_button_id.Margin = new System.Windows.Forms.Padding(4);
             this.customer_button_id.Name = "customer_button_id";
             this.customer_button_id.Size = new System.Drawing.Size(131, 28);
             this.customer_button_id.TabIndex = 0;
@@ -46,7 +48,7 @@
             // admin_button_id
             // 
             this.admin_button_id.Location = new System.Drawing.Point(588, 182);
-            this.admin_button_id.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.admin_button_id.Margin = new System.Windows.Forms.Padding(4);
             this.admin_button_id.Name = "admin_button_id";
             this.admin_button_id.Size = new System.Drawing.Size(100, 28);
             this.admin_button_id.TabIndex = 1;
@@ -54,18 +56,43 @@
             this.admin_button_id.UseVisualStyleBackColor = true;
             this.admin_button_id.Click += new System.EventHandler(this.admin_button_id_Click);
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(475, 308);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 20);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(475, 334);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(103, 20);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
             // connected
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.admin_button_id);
             this.Controls.Add(this.customer_button_id);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "connected";
             this.Text = "connected";
             this.Load += new System.EventHandler(this.connected_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,5 +100,7 @@
 
         private System.Windows.Forms.Button customer_button_id;
         private System.Windows.Forms.Button admin_button_id;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
