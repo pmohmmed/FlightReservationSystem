b8cf0e7c7c9461587ec6675d8f97aea8b703a7d2
Mohamed Ahmed <88492114+SEmohamedAhmed@users.noreply.github.com>
2023-04-29 22:02:41 +0200

    Add files via upload

--- FlightReservationSys/customer_cancel.Designer.cs
+++ FlightReservationSys/customer_cancel.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FlightReservationSys
 {
-    partial class customer_cancel_form
+    partial class customer_cancel
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,200 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sourceTB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.destinationTB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.priceTB = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimeTB = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cardNumTB = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.companyNameTB = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // customer_cancel_form
+            // comboBox1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(148, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(181, 24);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Booked-Flights-ID";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(306, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cancel Flight";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.cancel_flight);
+            // 
+            // sourceTB
+            // 
+            this.sourceTB.Location = new System.Drawing.Point(148, 222);
+            this.sourceTB.Name = "sourceTB";
+            this.sourceTB.ReadOnly = true;
+            this.sourceTB.Size = new System.Drawing.Size(181, 22);
+            this.sourceTB.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Source";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Destination";
+            // 
+            // destinationTB
+            // 
+            this.destinationTB.Location = new System.Drawing.Point(148, 268);
+            this.destinationTB.Name = "destinationTB";
+            this.destinationTB.ReadOnly = true;
+            this.destinationTB.Size = new System.Drawing.Size(181, 22);
+            this.destinationTB.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 317);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Price";
+            // 
+            // priceTB
+            // 
+            this.priceTB.Location = new System.Drawing.Point(148, 314);
+            this.priceTB.Name = "priceTB";
+            this.priceTB.ReadOnly = true;
+            this.priceTB.Size = new System.Drawing.Size(181, 22);
+            this.priceTB.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(391, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Date-Time";
+            // 
+            // dateTimeTB
+            // 
+            this.dateTimeTB.Location = new System.Drawing.Point(525, 219);
+            this.dateTimeTB.Name = "dateTimeTB";
+            this.dateTimeTB.ReadOnly = true;
+            this.dateTimeTB.Size = new System.Drawing.Size(181, 22);
+            this.dateTimeTB.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(391, 271);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Used Card Number";
+            // 
+            // cardNumTB
+            // 
+            this.cardNumTB.Location = new System.Drawing.Point(525, 268);
+            this.cardNumTB.Name = "cardNumTB";
+            this.cardNumTB.ReadOnly = true;
+            this.cardNumTB.Size = new System.Drawing.Size(181, 22);
+            this.cardNumTB.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(391, 320);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Company Name";
+            // 
+            // companyNameTB
+            // 
+            this.companyNameTB.Location = new System.Drawing.Point(525, 314);
+            this.companyNameTB.Name = "companyNameTB";
+            this.companyNameTB.ReadOnly = true;
+            this.companyNameTB.Size = new System.Drawing.Size(181, 22);
+            this.companyNameTB.TabIndex = 13;
+            // 
+            // customer_cancel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "customer_cancel_form";
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.companyNameTB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cardNumTB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimeTB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.priceTB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.destinationTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sourceTB);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "customer_cancel";
             this.Text = "customer_cancel";
-            this.Load += new System.EventHandler(this.customer_cancel_form_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.customer_cancel_FormClosing);
+            this.Load += new System.EventHandler(this.customer_cancel_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox sourceTB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox destinationTB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox priceTB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox dateTimeTB;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox cardNumTB;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox companyNameTB;
     }
 }
--- FlightReservationSys/customer_cancel.cs
+++ FlightReservationSys/customer_cancel.cs
@@ -1,25 +1,174 @@
-﻿using System;
+﻿using Oracle.DataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace FlightReservationSys
 {
-    public partial class customer_cancel_form : Form
+    public partial class customer_cancel : Form
     {
-        public customer_cancel_form()
+        string ordb = "Data source=xe;User Id=hr;Password=hr;";
+        OracleConnection conn;
+        int currentCustomerId = 1;
+        public customer_cancel()
         {
             InitializeComponent();
+            conn = new OracleConnection(ordb);
+            conn.Open();
         }
+        static List<Tuple<int,string>> databasedata = new List<Tuple<int, string>>();
 
-        private void customer_cancel_form_Load(object sender, EventArgs e)
+        // this function will show/load for the user his/her booked-flights in a combobox as flight id
+        // he can select one of the shown flights then the form will have some details about the flight
+        // then the user will determine wheather to cancel it or not
+        private void customer_cancel_Load(object sender, EventArgs e)
         {
-
+            Query_form_data_from_DB(); // put instead of 1 current customerID
         }
+        void Query_form_data_from_DB()
+        {
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT FLIGHT_ID,CARD_NUM FROM BOOKED_FLIGHTS WHERE CUSTOMER_ID = :customerId";
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("customerId", currentCustomerId);
+            OracleDataReader dr = cmd.ExecuteReader();
+            int flightId;
+            string cardNum;
+            while (dr.Read())
+            {
+                comboBox1.Items.Add(dr[0].ToString());  // flightId is shown in combobox
+                flightId = int.Parse(dr[0].ToString());
+                cardNum = dr[1].ToString();
+                databasedata.Add(new Tuple<int, string>(flightId, cardNum));
+            }
+            dr.Close();
+        }
+        void show_flight_details(List<object> flightData)
+        {
+            // flight details [0:source,1:destination,2:price,3:flightDate,4:cardNumUsed,5:companyName]  order does matter
+            if (flightData.Count != 6)
+            {
+                throw new Exception("flight details list in show_flight_details function has a problem with its count");
+                // or simply return from the function
+            }
+            // show flight details
+            sourceTB.Text = flightData[0].ToString();
+            destinationTB.Text = flightData[1].ToString();
+            priceTB.Text = flightData[2].ToString();
+            dateTimeTB.Text = flightData[3].ToString();
+            companyNameTB.Text = flightData[4].ToString();
+            cardNumTB.Text = flightData[5].ToString();
+        }
+        List<object> get_flight_data(int currentComboSeletedIdx)
+        {
+            // add 6 element to flight_data list        
+            int flightId = databasedata[currentComboSeletedIdx].Item1;  // or combobox.SelectedItem
+            List<object> flight_data = Query_flight_data_from_DB(flightId);
+            flight_data.Add(databasedata[currentComboSeletedIdx].Item2);    // adding card num
+            return flight_data;
+        }
+
+        List<object> Query_flight_data_from_DB(int flightId)
+        {
+            // thiis function returns list[0:source,1:destination,2:price,3:flightDate,4:companyName]
+            // Query flight data form DB using flight id and return it 
+            // SELECT * FROM FLIGHT_INFO WHERE FLIGHT_ID =: flightId => using Parameterized Query 
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT * FROM FLIGHT_INFO WHERE FLIGHT_ID =: flightId";
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("flightId", flightId);
+
+            OracleDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+
+            List<object> flightDetails = new List<object>();
+            flightDetails.Add(dr[2]);   //source
+            flightDetails.Add(dr[3]);   //destination
+            flightDetails.Add(dr[4]);   //price
+            flightDetails.Add(dr[6]);   //flightDate
+
+            int adminId = int.Parse(dr[1].ToString());
+
+            flightDetails.Add(get_company_name(adminId));//companyName
+
+            dr.Close();
+            return flightDetails;
+        }
+        string get_company_name(int adminId)
+        {
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT COMPANY_NAME FROM ADMIN_TABLE WHERE ADMIN_ID =: adminId";
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("adminId", adminId);
+
+            OracleDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+
+            string companyName = dr[0].ToString();
+
+            dr.Close();
+            return companyName;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var currentComboSeletedIdx = comboBox1.SelectedIndex;
+            var flightData = get_flight_data( currentComboSeletedIdx);
+            show_flight_details(flightData);
+        }
+
+        private void cancel_flight(object sender, EventArgs e)
+        {
+            // if cancelled button clicked raise new alert to confirm the cancellation
+            // if cancellation were confirmed then remove the data from the data base
+            if(comboBox1.SelectedIndex == -1)
+            {
+                return;
+            }
+            var confirmResult = MessageBox.Show("Are you sure to delete this flight ?","Confirm Delete!",MessageBoxButtons.OKCancel);
+            if (confirmResult == DialogResult.OK)
+            {
+                delete_booked_flight(databasedata[0].Item1, currentCustomerId);
+            }
+        }
+        void delete_booked_flight(int flightId, int customerId)
+        {
+            // delete flight from bookedFlights query using flightid and customer id = (primary key)
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "DELETE FROM BOOKED_FLIGHTS WHERE FLIGHT_ID =: flightId AND CUSTOMER_ID =: customerId";
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("flightId", flightId);
+            cmd.Parameters.Add("customerId", customerId);
+            cmd.ExecuteReader();        // delete query
+            clear_cancel_form();
+        }
+
+        void clear_cancel_form()
+        {
+            comboBox1.Items.Remove(comboBox1.Items[0]);
+            sourceTB.Text = string.Empty;
+            destinationTB.Text = string.Empty;
+            cardNumTB.Text = string.Empty;
+            dateTimeTB.Text = string.Empty;
+            priceTB.Text = string.Empty;
+            companyNameTB.Text = string.Empty;
+        }
+        private void customer_cancel_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            conn.Dispose();
+        }
+
     }
 }
