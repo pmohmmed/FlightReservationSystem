83dd4e2f13796d3ebe1a5d5caa3da180f44a0692
MohamedWalidxX <Moh.Walid2002@gmail.com>
2023-04-29 20:56:40 +0200

    CusCommit

---
+++ FlightReservationSys/cutomer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlightReservationSys
+{
+    public partial class cutomer : Form
+    {
+        public cutomer()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
